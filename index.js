3983d35cd917754860ab112d7e324698e529d4c2
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-28 20:43:54 +0300

    MVP completed

--- index.js
+++ index.js
@@ -22,11 +22,21 @@ function editUser(req,res) {
       }
     db.update(id,user)
       .then(data => {
-          res.json(data)
-          console.log("response from edit endpoint",data)
+        if (data === Number(0)) {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+            }
+            if (req.name || req.bio === undefined) {
+            res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+            console.log("response from edit",data)
+            }
+            else if ( data !== Number(0)) {
+            res.status(200).json({response: "user has been edited successfully",editedUser: user})
+            console.log("response from edit user endpoint",data)
+            }
       })
       .catch(error => {
-          console/log("error from edit endpoint",error)
+            res.status(500).json({ error: "The user information could not be modified." });
+          console.log("error from edit endpoint",error)
       })
 }
 
@@ -34,10 +44,16 @@ function deleteUser(req,res) {
     const {id} = req.params;
     db.remove(id)
     .then(data => {
-        res.json(data)
-        console.log("removed user",data)
+        if (data === Number(0)) {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+            }
+            else if ( data !== Number(0)) {
+            res.json({response: "user has been removed successfully"})
+            console.log("response from user endpoint",data)
+            }
     })
     .catch(error => {
+        res.status(500).json({ error: "The user could not be removed" });
         console.log("error from remove endpoint",error)
     })
 }
@@ -48,25 +64,45 @@ function postUser(req,res) {
         created_at: Date(Date.now()), 
         name: req.body.name
       }
+
       db.insert(user)
-      res.json(user)
-        .then(data => {
-            console.log("posted data",data)   
+      .then(data => {
+          console.log("posted data",data)   
+          res.status(201).json({newUSer:user})
         })
         .catch(error => {
+            if (req.name || req.bio === undefined) {
+                  res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+              }
+              else if (error.code !== "SQLITE_CONSTRAINT") {
+                   res.status(500).json({errorMessage:error.code,error: "There was an error while saving the user to the database"});
+              }
             console.log("error from post endpoint",error)
         })
 }
 
+// {
+//     "bio": "Not Tarzan's Wife, another Jane", 
+//    " created_at": "Mon Aug 14 2017 12:50:16 GMT-0700 (PDT)",
+//       "name": "Mildred",
+//       "named": "Mildred"
+  
+//   }
 
 function getUser(req,res)  {
     const { id } = req.params;
     db.findById(id)
       .then(data => {
+          if (data === undefined) {
+          res.status(404).json({ message: "The user with the specified ID does not exist." })
+          }
+          else if ( data !== undefined) {
           res.json(data)
           console.log("response from user endpoint",data)
+          }
       })  
       .catch(error => {
+        res.status(500).json({ error: "The users information could not be retrieved." })
           console.log("error from user endpoint",error)
       })
 }
@@ -79,7 +115,7 @@ function getAllUsers(req,res) {
       } ) 
       .catch(error => {
           console.log("error from server",error)
-          res.json(error)
+          res.status(500).json({ error: "The users information could not be retrieved." })
       }) 
 }
 
