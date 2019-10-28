44e305a19cd04e876e973f055d1daf048d78ede4
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-28 17:22:12 +0300

    did get users,get users by ID and post request

--- index.js
+++ index.js
@@ -6,10 +6,43 @@ const db = require("./data/db")
  server.use(cors())
  server.use(express.json())
 
-server.get("api/users/:id",getUser)
+ server.post("/api/users",postUser)
+server.get("/api/users/:id",getUser)
 server.get("/api/users",getAllUsers)
 server.get("*",handleRequest)
 
+function postUser(req,res) {
+    const user = {
+        bio: req.body.bio,  
+        created_at: Date(Date.now()), 
+        name: req.body.name
+      }
+      db.insert(user)
+        .then(data => {
+            console.log("posted data",data)
+            res.json(data)
+            
+        })
+        .catch(error => {
+            console.log("error from post endpoint",error)
+        })
+
+}
+
+// function createNewHub(req, res) {
+//     const hub = {
+//       name: req.body.name,
+//       created_at: 'now',
+//     }
+  
+//     db.add(hub)
+//       .then(data => {
+//         console.log(data);
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       })
+//   }
 
 function getUser(req,res)  {
     const { id } = req.params;
