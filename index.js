dcd20c58f441d9a7f1f43131e8225de6fa152c4a
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-28 16:40:33 +0300

    installed express and cors

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// implement your API here
+const express = require("express")
+const cors = require("cors")
+const server = express()
+const db = require("./data/db")
+
+ server.use(cors())
+ server.use(express.json())
+
+server.get("api/users/:id",getUser)
+server.get("/api/users",getAllUsers)
+server.get("*",handleRequest)
+
+
+function getUser(req,res)  {
+    const { id } = req.params;
+    db.findById(id)
+      .then(data => {
+          res.json(data)
+          console.log("response from user endpoint",data)
+      })  
+      .catch(error => {
+          console.log("error from user endpoint",error)
+      })
+}
+
+function getAllUsers(req,res) {
+    db.find()
+      .then(data => {
+          console.log("response from users endpoint",data)
+          res.json(data)
+      } ) 
+      .catch(error => {
+          console.log("error from server",error)
+          res.json(error)
+      }) 
+}
+
+function handleRequest(req,res) {
+    res.json("Hello from server!")
+}
+
+
+
+
+
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log("listening" + (process.env.PORT || 3000))
+} )
